f3211dea6254c3c135ee7052b0e299eaac2e225a
melvinchia3636 <melvinchia623600@gmail.com>
2022-05-23 18:02:27 +0800

    open file is now functional

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/no-children-prop */
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './Markdown.scss';
@@ -6,25 +14,54 @@ import { markdown } from '@codemirror/lang-markdown';
 import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkGemoji from 'remark-gemoji';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// @ts-ignore
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// @ts-ignore
+import remarkToc from 'remark-toc';
+import rehypeRaw from 'rehype-raw';
+
+import 'katex/dist/katex.min.css';
 
 const Main = () => {
   const [content, setContent] = useState('');
 
   useEffect(() => {
+    // @ts-ignore
     document.getElementById('close-btn').onclick = () => {
-      window.electron.ipcRenderer.sendMessage('ipc-example', ['closeWindow']);
+      window.electron.ipcRenderer.sendMessage('ipc', ['closeWindow']);
     };
+    // @ts-ignore
     document.getElementById('min-btn').onclick = () => {
-      window.electron.ipcRenderer.sendMessage('ipc-example', [
-        'minimizeWindow',
-      ]);
+      window.electron.ipcRenderer.sendMessage('ipc', ['minimizeWindow']);
     };
+    // @ts-ignore
     document.getElementById('max-btn').onclick = () => {
-      window.electron.ipcRenderer.sendMessage('ipc-example', [
-        'maximizeWindow',
-      ]);
+      window.electron.ipcRenderer.sendMessage('ipc', ['maximizeWindow']);
     };
-  });
+
+    window.electron.ipcRenderer.on('ipc', (args) => {
+      switch ((args as never)[0]) {
+        case 'open-file':
+          window.electron.ipcRenderer.sendMessage('ipc', ['openFileDialog']);
+          break;
+        case 'open-file-content':
+          window.electron
+            .readFile((args as never)[1])
+            .then((e) => setContent(e))
+            .catch((e) => console.log(e));
+          break;
+          case 'new-file':
+            window.electron.ipcRenderer.sendMessage('ipc', ['newFileConfirmation']);
+            break;
+        default:
+          break;
+      }
+    });
+  }, []);
 
   return (
     <div className="w-full h-screen flex flex-col">
@@ -37,7 +74,7 @@ const Main = () => {
         </div>
       </div>
       <div className="w-full h-full flex pt-[32px]">
-        <div className="w-1/2 h-full flex flex-col bg-black">
+        <div className="w-1/2 h-full flex flex-col bg-zinc-900 bg-opacity-70 border-r border-zinc-500 border-opacity-60">
           <CodeMirror
             value={content}
             theme="dark"
@@ -45,8 +82,48 @@ const Main = () => {
             onChange={setContent}
           />
         </div>
-        <div className="w-1/2 h-full overflow-y-auto overflow-x-hidden p-12 content bg-zinc-900 bg-opacity-60">
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        <div className="w-1/2 h-full overflow-y-auto overflow-x-hidden p-12 content bg-zinc-900 bg-opacity-70">
+          <ReactMarkdown
+            components={{
+              a: ({ href, children }) => (
+                <a
+                  aria-label={href}
+                  onClick={() => {
+                    window.electron.ipcRenderer.sendMessage('ipc', [
+                      'openExternal',
+                      href,
+                    ]);
+                  }}
+                >
+                  {children}
+                </a>
+              ),
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '');
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, '')}
+                    style={oneDark}
+                    customStyle={{
+                      backgroundColor: 'transparent',
+                      padding: '0 1rem',
+                    }}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  />
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+            remarkPlugins={[remarkGfm, remarkMath, remarkGemoji, remarkToc]}
+            rehypePlugins={[rehypeKatex, rehypeRaw]}
+          >
+            {content}
+          </ReactMarkdown>
         </div>
       </div>
     </div>
